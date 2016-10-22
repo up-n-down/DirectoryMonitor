d81e8558586db3bd92997eb142de7df61be67d75
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-10-22 14:12:17 +0200

    Initial commit

---
+++ Sources/DirectoryMonitor.swift
@@ -0,0 +1,4 @@
+struct DirectoryMonitor {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import DirectoryMonitorTests
+
+XCTMain([
+     testCase(DirectoryMonitorTests.allTests),
+])
