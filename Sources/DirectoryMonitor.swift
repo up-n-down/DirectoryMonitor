5273ec9a6fe5b0a877571f045b4316623aab7b4d
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-10-22 15:53:32 +0200

    Simplify and shorten property names

--- Sources/DirectoryMonitor.swift
+++ Sources/DirectoryMonitor.swift
@@ -17,66 +17,66 @@ class DirectoryMonitor {
     var fileDescriptor: CInt = -1
 
     /// A dispatch queue used for sending file changes in the directory.
-    let directoryMonitorQueue = DispatchQueue(label: "io.up-n-down.directorymonitor")
+    let queue = DispatchQueue(label: "io.up-n-down.directorymonitor")
 
     /// A dispatch source to monitor a file descriptor created from the directory.
-    var directoryMonitorSource: DispatchSourceProtocol?
+    var source: DispatchSourceProtocol?
 
     /// URL for the directory being monitored.
-    var URL: URL
+    var url: URL
 
     // MARK: Initializers
 
-    init(URL: URL) {
-        self.URL = URL
-        NSLog("Directory Monitor @ \(URL)")
+    init(url: URL) {
+        self.url = url
+        NSLog("Directory Monitor @ \(url)")
     }
 
     // MARK: Monitoring
 
     func startMonitoring() {
         // Listen for changes to the directory (if we are not already).
-        if directoryMonitorSource == nil && fileDescriptor == -1 {
+        if source == nil && fileDescriptor == -1 {
 
-            NSLog("Start monitoring \(URL) for changes.")
+            NSLog("Start monitoring \(url) for changes.")
 
             // Open the directory referenced by URL for monitoring only.
-            fileDescriptor = open(URL.path, O_EVTONLY)
+            fileDescriptor = open(url.path, O_EVTONLY)
 
             // Define a dispatch source monitoring the directory for additions, deletions, and renamings.
-            directoryMonitorSource = DispatchSource.makeFileSystemObjectSource(
+            source = DispatchSource.makeFileSystemObjectSource(
                 fileDescriptor: fileDescriptor,
                 eventMask: .write,
-                queue: directoryMonitorQueue
+                queue: queue
             )
 
             // Define the block to call when a file change is detected.
-            directoryMonitorSource?.setEventHandler(handler: {
+            source?.setEventHandler(handler: {
                 // Call out to the `DirectoryMonitorDelegate` so that it can react appropriately to the change.
-                self.delegate?.directoryMonitor(self, directoryDidChangeAt: self.URL)
+                self.delegate?.directoryMonitor(self, directoryDidChangeAt: self.url)
             })
 
             // Define a cancel handler to ensure the directory is closed when the source is cancelled.
-            directoryMonitorSource?.setCancelHandler(handler: {
+            source?.setCancelHandler(handler: {
                 close(self.fileDescriptor)
 
                 self.fileDescriptor = -1
 
-                self.directoryMonitorSource = nil
+                self.source = nil
             })
 
             // Start monitoring the directory via the source.
-            directoryMonitorSource?.resume()
+            source?.resume()
         }
     }
 
     func stopMonitoring() {
         // Stop listening for changes to the directory, if the source has been created.
-        if directoryMonitorSource != nil {
+        if source != nil {
             // Stop monitoring the directory via the source.
-            directoryMonitorSource?.cancel()
+            source?.cancel()
         }
         
-        NSLog("Stop monitoring \(URL) for changes.")
+        NSLog("Stop monitoring \(url) for changes.")
     }
 }
